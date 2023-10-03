7caf0f5136a674f961da9d249c36d0463788d621
Edgar Moises Sanchez Reyes <edgar_95_san@hotmail.com>
2023-10-02 22:23:00 -0700

    3- Se crea el repository y clase servicio crud, y el controlador producto y la vista para ver el listado de productos

---
+++ src/main/java/com/proyecto/cocona/repository/ProductoRepository.java
@@ -0,0 +1,13 @@
+/*se crea una interfaz para hacer el crud de los productos */
+
+package com.proyecto.cocona.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.proyecto.cocona.modelo.Producto;
+
+@Repository                                                      //la interfaz extiende a JpaRepository para la clase producto
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+    
+}
---
+++ src/main/java/com/proyecto/cocona/servicio/ProductoServicio.java
@@ -0,0 +1,19 @@
+package com.proyecto.cocona.servicio;
+
+import java.util.Optional;
+
+import com.proyecto.cocona.modelo.Producto;
+
+//en esta interfaz se define todos los metodos crud para la clase producto
+
+public interface ProductoServicio {
+
+    public Producto save(Producto producto);
+
+    public Optional<Producto> get (Integer id);
+
+    public void update(Producto producto);
+
+    public void delete(Integer id);
+
+}
