7e91f623ff1aab178d6a805e2dc9cb07210fde57
Edgar Moises Sanchez Reyes <edgar_95_san@hotmail.com>
2023-10-03 23:08:27 -0700

    Se crea la vista para crear producto y el menu para el administrador

--- src/main/java/com/proyecto/cocona/controlador/ProductoController.java
+++ src/main/java/com/proyecto/cocona/controlador/ProductoController.java
@@ -10,6 +10,11 @@ public class ProductoController {
     
     @GetMapping("")
     public String show(){
-        return "productos/show";
+        return "productos/show";  //nos redirige al show.html
     }
+
+    @GetMapping("/create")
+    public String create(){
+        return "productos/create";  //nos redirige al create.html
+    } 
 }
