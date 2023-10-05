b1fb745bb17cf22b721113899749077b58ad7406
Edgar Moises Sanchez Reyes <edgar_95_san@hotmail.com>
2023-10-04 23:02:03 -0700

    Se implementa el metodo guardar producto en la base de datos

--- src/main/java/com/proyecto/cocona/controlador/ProductoController.java
+++ src/main/java/com/proyecto/cocona/controlador/ProductoController.java
@@ -2,18 +2,25 @@ package com.proyecto.cocona.controlador;
 
 
 import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.proyecto.cocona.modelo.Producto;
+import com.proyecto.cocona.modelo.Usuario;
+import com.proyecto.cocona.servicio.ProductoServicio;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
     
     private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
+
+    @Autowired
+    private ProductoServicio productoServicio;
+
     @GetMapping("")
     public String show(){
         return "productos/show";  //nos redirige al show.html
@@ -27,6 +34,9 @@ public class ProductoController {
     @PostMapping("/save")
     public String save(Producto producto){  //metodo para guardar el producto a la base de datos
         LOGGER.info("Este es el objeto producto {}", producto);
+        Usuario u = new Usuario(1, "", "", "", "", "", "", "");
+        producto.setUsuario(u);
+        productoServicio.save(producto);
         return "redirect:/productos";
     }
 }
