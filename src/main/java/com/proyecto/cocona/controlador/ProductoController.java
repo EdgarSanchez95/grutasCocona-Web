8069dddb423818399750c0c1231aeefbb59a9b2b
Edgar Moises Sanchez Reyes <edgar_95_san@hotmail.com>
2023-10-04 22:27:37 -0700

    Se crea el metodo guardar producto y el Logger para el controller

--- src/main/java/com/proyecto/cocona/controlador/ProductoController.java
+++ src/main/java/com/proyecto/cocona/controlador/ProductoController.java
@@ -1,13 +1,19 @@
 package com.proyecto.cocona.controlador;
 
+
+import org.slf4j.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.proyecto.cocona.modelo.Producto;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
     
+    private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
     @GetMapping("")
     public String show(){
         return "productos/show";  //nos redirige al show.html
@@ -17,4 +23,10 @@ public class ProductoController {
     public String create(){
         return "productos/create";  //nos redirige al create.html
     } 
+
+    @PostMapping("/save")
+    public String save(Producto producto){  //metodo para guardar el producto a la base de datos
+        LOGGER.info("Este es el objeto producto {}", producto);
+        return "redirect:/productos";
+    }
 }
