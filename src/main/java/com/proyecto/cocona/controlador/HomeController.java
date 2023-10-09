dead499d959ba9c4bbc1a9a8d1ca1c3dc114686d
Edgar Moises Sanchez Reyes <edgar_95_san@hotmail.com>
2023-10-08 22:58:21 -0700

    Se crea la funcionalidad para mostrar los datos del producto en la vista productohome

--- src/main/java/com/proyecto/cocona/controlador/HomeController.java
+++ src/main/java/com/proyecto/cocona/controlador/HomeController.java
@@ -1,5 +1,7 @@
 package com.proyecto.cocona.controlador;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.proyecto.cocona.modelo.Producto;
 import com.proyecto.cocona.servicio.ProductoServicio;
 
 @Controller
@@ -28,9 +31,14 @@ public class HomeController {
     }
 
     @GetMapping("productohome/{id}")
-    public String productoHome(@PathVariable Integer id){
+    public String productoHome(@PathVariable Integer id, Model model){
 
         log.info("id producto enviado como parametro {}", id);
+        Producto producto = new Producto();
+        Optional<Producto> productoOptional = productoServicio.get(id);
+        producto = productoOptional.get();
+
+        model.addAttribute("producto", producto);
         
         return "usuario/productohome";
     }
