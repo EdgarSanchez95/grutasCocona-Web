f1af0c9bd5f810e8a39f0d83d989adece9bf2e90
Edgar Moises Sanchez Reyes <edgar_95_san@hotmail.com>
2023-10-08 20:02:15 -0700

    Se muestran los datos del producto en otra vista

--- src/main/java/com/proyecto/cocona/controlador/HomeController.java
+++ src/main/java/com/proyecto/cocona/controlador/HomeController.java
@@ -1,9 +1,12 @@
 package com.proyecto.cocona.controlador;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.proyecto.cocona.servicio.ProductoServicio;
@@ -12,6 +15,8 @@ import com.proyecto.cocona.servicio.ProductoServicio;
 @RequestMapping("/")
 public class HomeController {
     
+    private final Logger log = LoggerFactory.getLogger(HomeController.class);
+
      @Autowired
      private ProductoServicio productoServicio;
 
@@ -20,5 +25,13 @@ public class HomeController {
        
         model.addAttribute("productos", productoServicio.findAll());
         return "usuario/home";
+    }
+
+    @GetMapping("productohome/{id}")
+    public String productoHome(@PathVariable Integer id){
+
+        log.info("id producto enviado como parametro {}", id);
+        
+        return "usuario/productohome";
     }
 }
