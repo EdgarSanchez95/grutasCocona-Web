3ab0bb33b6a8e5e32121478ed5f85b3be1e675ed
Edgar Moises Sanchez Reyes <edgar_95_san@hotmail.com>
2023-10-15 16:15:22 -0700

    Se crea el metodo para generar el  numero de la orden

--- src/main/java/com/proyecto/cocona/servicio/IOrdenServicio.java
+++ src/main/java/com/proyecto/cocona/servicio/IOrdenServicio.java
@@ -1,7 +1,10 @@
 package com.proyecto.cocona.servicio;
 
+import java.util.List;
+
 import com.proyecto.cocona.modelo.Orden;
 
 public interface IOrdenServicio {
+    List<Orden> findAll();
     Orden save  (Orden orden);
 }
