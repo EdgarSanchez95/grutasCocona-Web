b3dec3694dc54dcac7746ae95e1292aa92ce30ad
Edgar Moises Sanchez Reyes <edgar_95_san@hotmail.com>
2023-10-21 17:56:25 -0700

    Mostrar el detalle de compras para el usuario

--- src/main/java/com/proyecto/cocona/servicio/IOrdenServicio.java
+++ src/main/java/com/proyecto/cocona/servicio/IOrdenServicio.java
@@ -1,13 +1,16 @@
 package com.proyecto.cocona.servicio;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.proyecto.cocona.modelo.Orden;
 import com.proyecto.cocona.modelo.Usuario;
 
 public interface IOrdenServicio {
     List<Orden> findAll();
+    Optional<Orden> findById(Integer id);
     Orden save  (Orden orden);
     String generarNumeroOrden();
     List<Orden> findByUsuario(Usuario usuario);
+
 }
